40be3923efdd30c8127aa794cbec96c226344e7c
Zahid <zahidurrehman789@gmail.com>
2026-06-15 00:41:55 +0500

    fixed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            RolesAndPermissionsSeeder::class,
-            UserSeeder::class,
+            // RolesAndPermissionsSeeder::class,
+            // UserSeeder::class,
             /*CustomerSeeder::class,
             ProductsSeeder::class,
             GuarantorsSeeder::class,
